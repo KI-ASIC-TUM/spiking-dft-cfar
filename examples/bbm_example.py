cca735e27a1b63d991a7250c02a4a8b326d1a80e
javier <lopez.randulfe@tum.de>
2020-10-28 11:07:36 +0100

    Add reader library and test it in basic example

--- examples/bbm_example.py
+++ examples/bbm_example.py
@@ -5,9 +5,12 @@ Main description of the module.
 # Standard libraries
 # Local libraries
 import snn_dft_cfar.pipeline
+import snn_dft_cfar.utils.read_data
 
 
 def main(filename="../data/BBM/samples_ch_1_scenario2.txt"):
+    # Only the 900 first samples contain information
+    data_cube = snn_dft_cfar.utils.read_data.bbm_get_datacube(filename)[:, :900]
     snn_dft_cfar.pipeline.pipeline()
 
 

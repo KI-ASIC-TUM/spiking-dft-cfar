cc2120fc702628cf410fcfaf472b5f45c27dffd5
Tobias Duswald <tobias.duswald@tum.de>
2020-11-02 13:16:54 +0100

    Implemented OS / CA CFAR plus test in 1d (#3)

--- snn_dft_cfar/cfar.py
+++ snn_dft_cfar/cfar.py
@@ -7,23 +7,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 # Local libraries
 
-# class CFAR(ABC):
-#     """
-#     Encoder abstract class
-# 
-#     Any encoder to be implemented shall be created as an instance of
-#     this class
-#     """
-#     def __init__(self):
-#         return
-# 
-#     @abstractmethod
-#     def run(self, data, *args):
-#         return data
-# 
-#     def __call__(self, data, *args):
-#         return self.run(data, *args)
-
 
 class TraditionalCFAR(ABC):
     """
@@ -50,15 +33,19 @@ class TraditionalCFAR(ABC):
         # Initialize arrays for input and output
         self.input_array = np.empty(1)
         self.results = np.empty(1)
+        self.threshold = np.empty(1)
         
     def __call__(self, data, *args):
+        """
+        Calling the object executes the function 'run'.
+        """
         return self.run(data, *args)
         
     @abstractmethod
     def statistical_measure(self,np_array):
         """
         Computes the statistical measure for the CFAR algorithm.
-        CACFAR: average; OSCFAR: k-th largest
+        CACFAR: average; OSCFAR: k-th largest.
         """
         return .5
 
@@ -83,7 +70,7 @@ class TraditionalCFAR(ABC):
         neighbours[:self.neighbour_cells] = np_array[:self.neighbour_cells]
         neighbours[self.neighbour_cells:] = np_array[self.neighbour_cells+
                                              2*self.guarding_cells + 1:]
-        test_value = np_array[self.neighbour_cells+self.guarding_cells+1]
+        test_value = np_array[self.neighbour_cells+self.guarding_cells]
         return test_value, neighbours
 
     
@@ -93,18 +80,50 @@ class TraditionalCFAR(ABC):
 
         @param np_array: np.array with ndim == 1
         """
+
+        # compute sizes related to sliding window 
         N = np_array.size
         window_size = 2*self.neighbour_cells + 2*self.guarding_cells + 1
         total_windows = N-window_size+1
-
+        
+        # initialize solution arrays
         self.results = np.zeros(total_windows)
+        self.threshold = np.zeros_like(self.results)
 
+        # DEBUG
+        #print('input array')
+        #print(np_array)
+
+        # iterate in sliding window fashion over input array
         for i in range(total_windows):
+            # determine regions of interest
             test_value, neighbour_values = self.roi_1d(
                                             np_array[i:i+window_size])
+            # compute statistical measure
             stat = self.statistical_measure(neighbour_values)
+            # save threshold
+            self.threshold[i] = stat / self.scale_factor
+            # save results
             self.results[i] = self.compare(test_value,stat)
 
+            # DEBUG
+            #print()
+            #print('step')
+            #print(i)
+            #print('result')
+            #print(self.results[i])
+            #print('input')
+            #print(np_array[i:i+window_size])
+            #print('neighbours')
+            #print(neighbour_values)
+            #print('test value')
+            #print(test_value)
+            #print('scaled test value')
+            #print(self.scale_factor*test_value)
+            #print('statistical value')
+            #print(stat)
+
+        # return results array    
         return self.results
       
     def cfar_2d(self,np_array):
@@ -156,10 +175,34 @@ class TraditionalCFAR(ABC):
         """
         Visualize the 1D input and output data.
         """
+
+        # plot input data
         plt.plot(self.input_array,label = 'signal')
+
+        # plot threshold line
+        low = self.guarding_cells+self.neighbour_cells
+        high = self.guarding_cells+self.neighbour_cells+self.threshold.size
+        plt.plot(range(low,high),self.threshold,ls='dotted',lw=1, c='C3', 
+                 label='threshold')
+
+        # plot detected peaks
+        cntr = 0
         for x in np.where(self.results>=1)[0]:
-            plt.axvline(x+self.guarding_cells+self.neighbour_cells,ls='--',
-                        c='C1',lw=2)
+            if cntr == 0:
+                plt.axvline(x+self.guarding_cells+self.neighbour_cells,ls='--',
+                            c='C1',lw=1,label='detected peaks')
+                cntr +=1
+            else:
+                plt.axvline(x+self.guarding_cells+self.neighbour_cells,ls='--',
+                            c='C1',lw=1)
+
+        # plot boundaries where algorithm works properly
+        plt.axvline(self.guarding_cells+self.neighbour_cells-1,ls='--',
+                    c='C2',lw=1,label='algorithm boundaries')
+        plt.axvline(self.results.size+self.guarding_cells+self.neighbour_cells,
+                    ls='--', c='C2',lw=1)
+        
+        # show plot
         plt.legend()
         plt.grid(True)
         plt.ylabel('signal')
@@ -171,7 +214,40 @@ class CACFAR(TraditionalCFAR):
     Cell Averaging CFAR algorithms.
     """
     def __init__(self, scale_factor,guarding_cells,neighbour_cells):
+        """
+        Initialization.
+
+        @param scale_factor: how to scale test_cell before compring with 
+                             statistical measure
+        @param guarding_cells: number of guarding cells (counted from left to 
+                               center)
+        @param neighbour_cells: number of neighbour cells (as above)
+        """
         super(CACFAR,self).__init__(scale_factor,guarding_cells,neighbour_cells)
 
     def statistical_measure(self,np_array):
         return np.average(np_array)
+
+
+class OSCFAR(TraditionalCFAR):
+    """
+    Ordered Statistics CFAR algorithms.
+    """
+    def __init__(self, scale_factor,guarding_cells,neighbour_cells,k):
+        """
+        Initialization.
+
+        @param scale_factor: how to scale test_cell before compring with 
+                             statistical measure
+        @param guarding_cells: number of guarding cells (counted from left to 
+                               center)
+        @param neighbour_cells: number of neighbour cells (as above)
+        @param k: k-th largest value to find for statistical measure (int)
+        """
+        super(OSCFAR,self).__init__(scale_factor,guarding_cells,neighbour_cells)
+        # OSCFAR needs an integer k for determining the k-th largest value
+        self.k = k
+
+    def statistical_measure(self,np_array):
+        return np.partition(np_array,-self.k)[-self.k]
+        
--- test/traditional_cfar.py
+++ test/traditional_cfar.py
@@ -1,24 +1,44 @@
 import numpy as np
-from snn_dft_cfar.cfar import CACFAR
+from snn_dft_cfar.cfar import CACFAR, OSCFAR
 
 # define parameters
 data_size = 50
 data_peaks = 3 
 guarding_cells = 2
 neighbour_cells = 7
-scale_factor = 0.65
+scale_factor = 0.5
+k = 4
+random_seed = 4
+
+# print test information
+print(34*"=","CFAR test", 34*"=")
+print("Test to see if CFAR works correctly.")
+print()
+print("Parameters:")
+print("number of peaks : {}".format(data_peaks))
+print("guarding cells  : {}".format(guarding_cells))
+print("neighbour cells : {}".format(neighbour_cells))
+print("scaling factor  : {}".format(scale_factor))
+print("k               : {}".format(k))
+print()
+print("First  plot: CACFAR")
+print("Second plot: OSCFAR")
+print((68+11)*"=")
 
 # generate data with peaks
-rng = np.random.default_rng()
+rng = np.random.default_rng(seed=random_seed)
 data = rng.random(data_size)
 for i in rng.integers(data_size, size=data_peaks):
     data[i] += 1
 
 # initialize CFAR 
 cacfar = CACFAR(scale_factor,guarding_cells,neighbour_cells)
+oscfar = OSCFAR(scale_factor,guarding_cells,neighbour_cells,k)
 
 # run CFAR
 cacfar(data)
+oscfar(data)
 
 # visualize CFAR
 cacfar.plot()
+oscfar.plot()
